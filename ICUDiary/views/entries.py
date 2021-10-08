d168463c24d813c0c5506b124474ae4739b7a507
Will Meng <wimeng@umich.edu>
2021-10-08 14:18:01 -0400

    added logo, made mocked archive data for presentation

--- ICUDiary/views/entries.py
+++ ICUDiary/views/entries.py
@@ -20,7 +20,8 @@ def newentry():
         return flask.redirect("/accounts/login/")
     return flask.render_template("recording.html")
 
-@ICUDiary.app.route("/archive/")
+# THE REAL ENDPOINT FOR THIS IS /archive/, THIS IS TEMPORARILY SET FOR THE DEMO
+@ICUDiary.app.route("/archive/1/")
 def archive():
     """Send file."""
 
@@ -44,7 +45,26 @@ def archive():
 
     return flask.render_template("archive.html")
 
+@ICUDiary.app.route("/archive/")
+def archive1():
+    """Send file."""
 
-# def logged():
-#     """User logged in check."""
-#     return "user" in flask.session
+    if logged() is False:
+        return flask.redirect("/accounts/login/")
+
+    # authenticate that only patient and superuser can view archive
+    connect = ICUDiary.model.get_db()
+
+    role = connect.execute(
+        "SELECT role "
+        "FROM users "
+        "WHERE username = ?", (flask.session["user"],)
+    )
+    
+    curr_role = role.fetchall()[0]['role']
+    print(curr_role)
+
+    if curr_role == "User":
+        abort(403)
+
+    return flask.render_template("archive-hardcoded1.html")
