eb3a25cc21acc3fce6475d9a0b47da7597f22d8a
Will Meng <wimeng@umich.edu>
2021-10-25 13:36:42 -0400

    adding role to common context

--- ICUDiary/views/entries.py
+++ ICUDiary/views/entries.py
@@ -21,11 +21,12 @@ def common_context():
     if 'user' in flask.session:
         context['user'] = flask.session['user']
         cur = connect.execute(
-            "SELECT filename FROM users "
+            "SELECT filename, role FROM users "
             "WHERE username = ? ", (flask.session["user"],)
         )
         photo = cur.fetchall()
         context['filename'] = photo[0]['filename']
+        context['role'] = photo[0]['role']
     return context
 
 @ICUDiary.app.route("/newentry/", methods=['POST', 'GET'])
